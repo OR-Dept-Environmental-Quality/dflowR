177fa52ff4362ee8c7d2b12c45aa28f5d6eb813a
rmichie <michie.ryan@deq.state.or.us>
2017-12-20 12:57:58 -0800

    doc update

--- dflow_example.R
+++ dflow_example.R
@@ -11,7 +11,7 @@ q <- q.df[,c(3,4)]
 colnames(q) <-c("date", "flow")
 q$date <- as.POSIXct(q$date, format="%m/%d/%Y")
 
-dflow(x=q, m=7, R=10, wy.start="10-01", wy.end="09-30")
+dflow(x=q, m=7, R=10, period.start=NA, period.end=NA, wy.start="10-01", wy.end="09-30")
 
 # Example 2
 q.df  <- read.table(file= "C:/Program Files (x86)/DFLOW 3.1b/usgs02072500.txt", header = TRUE, sep ="\t", 
@@ -20,7 +20,7 @@ q <- q.df[,c(3,4)]
 colnames(q) <-c("date", "flow")
 q$date <- as.POSIXct(q$date, format="%m/%d/%Y")
 
-dflow(x=q, m=7, R=10, wy.start="10-01", wy.end="09-30")
+dflow(x=q, m=7, R=10, period.start=NA, period.end=NA, wy.start="10-01", wy.end="09-30")
 
 # Example 3
 q.df  <- read.table(file= "C:/Program Files (x86)/DFLOW 3.1b/usgs02078000.txt", header = TRUE, sep ="\t", 
@@ -29,7 +29,7 @@ q <- q.df[,c(3,4)]
 colnames(q) <-c("date", "flow")
 q$date <- as.POSIXct(q$date, format="%m/%d/%Y")
 
-dflow(x=q, m=7, R=10, wy.start="10-01", wy.end="09-30")
+dflow(x=q, m=7, R=10, period.start=NA, period.end=NA, wy.start="10-01", wy.end="09-30")
 
 #--- USGS web download example ------------------------------------------------------------------------------------
 
@@ -42,25 +42,29 @@ q.df <- readNWISdv(siteNumbers = "14174000",
                    endDate = "2016-09-30",
                    statCd = "00003")
 
+# Just get columns 3 and 4 (date and flow)
 q <- q.df[,c(3,4)]
 colnames(q) <-c("date", "flow")
 q$date <- as.POSIXct(q$date, format="%Y-%m-%d")
 
-dflow(x=q, m=7, R=10, wy.start="10-01", wy.end="09-30")
+dflow(x=q, m=7, R=10, period.start=NA, period.end=NA, wy.start="10-01", wy.end="09-30")
 
 #--- OWRD web download example ------------------------------------------------------------------------------------
 
 require(RCurl)
 
-station <- "14120000"
+stationID <- "14120000"
 start.date <- "10/1/1897"
 end.date <- "9/30/2016"
+data.type <- "MDF" # Mean Daily Flow
 
-# create url
-d.url <- paste0("http://apps.wrd.state.or.us/apps/sw/hydro_near_real_time/hydro_download.aspx?station_nbr=",station,
-                "&start_date=",start.date,
-                "%2012:00:00%20AM&end_date=",end.date,
-                "%2012:00:00%20AM&dataset=MDF&format=tab")
+# create download url
+d.url <- paste0("http://apps.wrd.state.or.us/apps/sw/hydro_near_real_time/hydro_download.aspx?",
+                "station_nbr=",stationID,
+                "&start_date=",start.date,"%2012:00:00%20AM",
+                "&end_date=",end.date,"%2012:00:00%20AM",
+                "&dataset=",data.type,
+                "&format=tab")
 
 
 url.content <- getURL(d.url)
@@ -68,8 +72,10 @@ url.content <- getURL(d.url)
 q.df <- read.table(file=textConnection(url.content), header = TRUE, sep ="\t", 
                    skip=0, stringsAsFactors=FALSE )
 
+# get columns 2 and 3 (date and flow)
 q <- q.df[,c(2,3)]
+
 colnames(q) <-c("date", "flow")
 q$date <- as.POSIXct(q$date, format="%m-%d-%Y")
 
-dflow(x=q, m=7, R=10, wy.start="10-01", wy.end="09-30")
+dflow(x=q, m=7, R=10, period.start=NA, period.end=NA, wy.start="10-01", wy.end="09-30")
